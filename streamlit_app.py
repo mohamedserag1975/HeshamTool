b63b6eee657dd0fae5d71e678167e45d5b7ae5ff
mohamedserag1975 <51572988+mohamedserag1975@users.noreply.github.com>
2023-05-29 02:54:26 +0400

    Update
    Update to menus

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,62 +3,46 @@ import pandas as pd
 import my_functions
 import time
 import streamlit as st
-#import plotly.graph_objs as go
+import plotly.graph_objs as go
 import pandas as pd
-#import plotly.express as px
+import plotly.express as px
 import warnings
 from PIL import Image
 import datetime as datetime
-#from streamlit_option_menu import option_menu
-#from scipy import stats
+from streamlit_option_menu import option_menu
+from scipy import stats
 import xlrd as xlrd
-# import openpyxl as openpyxl
 
 st.set_page_config(page_title="", page_icon="",
                    initial_sidebar_state="expanded", menu_items={"About": "Rev 0 - M.Serag"}, layout="wide")
 
-col1, col2, col3, col4 = st.columns(4, gap="large")
-with col1:
-    uploaded_file_time = st.file_uploader("Time and Cost", type="XLS", accept_multiple_files=False, key="Time", help=None,
-                                          on_change=None, args=None,
-                                          kwargs=None, disabled=False, label_visibility="visible")
-with col2:
-    uploaded_file_docs = st.file_uploader("Documents", type="XLSX", accept_multiple_files=False, key="Doc", help=None,
-                                          on_change=None, args=None,
-                                          kwargs=None, disabled=False, label_visibility="visible")
-with col3:
-    uploaded_file_invoices = st.file_uploader("Invoicing", type="XLS", accept_multiple_files=False, key="invoice", help=None,
-                                          on_change=None, args=None,
-                                          kwargs=None, disabled=False, label_visibility="visible")
-with col4:
-    st.write()
+df = my_functions.get_data_from_excel("expenses.xls")
+df.dropna(how='all', axis=1, inplace=True)
 
-if uploaded_file_time is not None:
-    df = my_functions.get_data_from_excel(uploaded_file_time)
-    df.dropna(how='all', axis=1, inplace=True)
-if uploaded_file_docs is not None:
-    df1 = my_functions.get_data_from_excel(uploaded_file_docs)
-    df1.dropna(how='all', axis=1, inplace=True)
-if uploaded_file_invoices is not None:
-    df2 = my_functions.get_data_from_excel(uploaded_file_invoices)
-    df2.dropna(how='all', axis=1, inplace=True)
+df1 = my_functions.get_data_from_excel("doc_status.xlsx")
+df1.dropna(how='all', axis=1, inplace=True)
 
-    df = df[(df["Project No."] == "15195-002") | (df["Project No."] == "15195-008")]
-    df[['Discipline', 'Phase' ]] = df['Task'].str.split('-', expand=True)
-    df = df.fillna("NA")
-    # df['discipline_Name'] =
-    totalhoursAD = df.groupby([(df["Transaction Type"] == "Time"), (df["Project No."] == "15195-002")])["Quantity Reg."].sum()
-    totalcostAD = df.groupby([(df["Transaction Type"] == "Time"), (df["Project No."] == "15195-002")])["Cost, Reg."].sum()
-    totalhoursPAK = df.groupby([(df["Transaction Type"] == "Time"), (df["Project No."] == "15195-008")])["Quantity Reg."].sum()
-    totalCostPAK = df.groupby([(df["Transaction Type"] == "Time"), (df["Project No."] == "15195-008")])["Cost, Reg."].sum()
-    total_to_invoice = df2["Amount, Currency"].sum()
-    total_invoiced = df2[df2["Transferred"] == True]["Amount, Currency"].sum()
+df2 = my_functions.get_data_from_excel("Invoicing.xls")
+df2.dropna(how='all', axis=1, inplace=True)
 
-    showcards = st.checkbox('show Score Cards')
-    if showcards:
-        # st.dataframe(df)
-        st.header("Timesheet Summary")
-        exchangeratePKR = st.number_input("Please input the exchange rate from USD to PKR, 1 USD = xxx PKR",value=285.08)
+df = df[(df["Project No."] == "15195-002") | (df["Project No."] == "15195-008")]
+df[['Discipline', 'Phase' ]] = df['Task'].str.split('-', expand=True)
+df = df.fillna("NA")
+# df['discipline_Name'] =
+totalhoursAD = df.groupby([(df["Transaction Type"] == "Time"), (df["Project No."] == "15195-002")])["Quantity Reg."].sum()
+totalcostAD = df.groupby([(df["Transaction Type"] == "Time"), (df["Project No."] == "15195-002")])["Cost, Reg."].sum()
+totalhoursPAK = df.groupby([(df["Transaction Type"] == "Time"), (df["Project No."] == "15195-008")])["Quantity Reg."].sum()
+totalCostPAK = df.groupby([(df["Transaction Type"] == "Time"), (df["Project No."] == "15195-008")])["Cost, Reg."].sum()
+total_to_invoice = df2["Amount, Currency"].sum()
+total_invoiced = df2[df2["Transferred"] == True]["Amount, Currency"].sum()
+
+menumain = option_menu("", ["Time & Cost", 'Document', "Invoices"], default_index=0, orientation="horizontal")
+
+if menumain == "Time & Cost":
+    timecheckbox = st.checkbox("Show detailed analysis")
+    if not timecheckbox:
+
+        exchangeratePKR = st.number_input("USD to PKR, 1 USD = xxx PKR",value=285.08)
         exchangerateAED = 3.765
         col100, col200, col300 = st.columns(3, gap="large")
         with col100:
@@ -84,8 +68,7 @@ if uploaded_file_invoices is not None:
         my_functions.style_metric_cards(border_left_color="black", background_color="lightgrey", border_size_px=0,
                                         border_color="lightgrey")
 
-    showtimedetails = st.checkbox('show Time&Cost details')
-    if showtimedetails:
+    else:
         lst_projectno = my_functions.unique(df, "Project No.")
         lst_phase = my_functions.unique(df, 'Phase')
         lst_discipline = my_functions.unique(df, 'Discipline')
@@ -139,42 +122,41 @@ if uploaded_file_invoices is not None:
 
         st.dataframe(df_filtered, width = 3 *800)
 
-        # Document analysis
-    showdocanalysis = st.checkbox("Show Document Data")
-    if showdocanalysis:
+    # Document analysis
+if menumain == "Document":
 
-        doc_name = "LOD:DocNo-DocTitle"
-        total_docs_count = df1[doc_name].nunique()
-        total_disc = df1["LOD:DC*"].nunique()
+    doc_name = "LOD:DocNo-DocTitle"
+    total_docs_count = df1[doc_name].nunique()
+    total_disc = df1["LOD:DC*"].nunique()
 
-        total_docs_submitted = df1[(df1["W-Status"] == "Completed/ Submitted") & (df1["Rev"] == "C1")][doc_name].nunique()
-        total_docs_approved = df1[(df1["Doc-Status"] == "IFU")&(df1["W-Status"] == "Completed/ Submitted")][doc_name].nunique()
-        doc_3_rev_filter = df1[doc_name].value_counts().rename('rev_counts')
-        df1 = df1.merge(doc_3_rev_filter.to_frame(),
-                                        left_on=doc_name,
-                                        right_index=True)
-        doc_3_rev = df1[df1.rev_counts > 3][[doc_name, "rev_counts"]].drop_duplicates()
-        doc_3_rev_total = df1[df1.rev_counts > 3][doc_name].nunique()
-        doc_per_disc = df1.drop_duplicates(subset=doc_name, keep="first")
-        doc_per_disc = doc_per_disc["LOD:DC*"].value_counts()
-        total_docs_rejected = df1[(df1["Review Code"] == "E")][doc_name].nunique()
+    total_docs_submitted = df1[(df1["W-Status"] == "Completed/ Submitted") & (df1["Rev"] == "C1")][doc_name].nunique()
+    total_docs_approved = df1[(df1["Doc-Status"] == "IFU")&(df1["W-Status"] == "Completed/ Submitted")][doc_name].nunique()
+    doc_3_rev_filter = df1[doc_name].value_counts().rename('rev_counts')
+    df1 = df1.merge(doc_3_rev_filter.to_frame(),
+                                    left_on=doc_name,
+                                    right_index=True)
+    doc_3_rev = df1[df1.rev_counts > 3][[doc_name, "rev_counts"]].drop_duplicates()
+    doc_3_rev_total = df1[df1.rev_counts > 3][doc_name].nunique()
+    doc_per_disc = df1.drop_duplicates(subset=doc_name, keep="first")
+    doc_per_disc = doc_per_disc["LOD:DC*"].value_counts()
+    total_docs_rejected = df1[(df1["Review Code"] == "E")][doc_name].nunique()
 
 
-        st.write("Total Documents count", total_docs_count)
-        st.write("Total No of discipline", total_disc)
-        st.write("Total documents submitted", total_docs_submitted, "--> ",
-                 (round((total_docs_submitted/total_docs_count)*100,1)), "%")
-        st.write("Total Approved documents", total_docs_approved)
-        st.write("Total Documents with more than 3 revisions",doc_3_rev_total)
-        st.write("rejected",total_docs_rejected)
-        col1, col2 = st.columns(2, gap="small")
-        with col1:
-            st.subheader("Documents per discipline")
-            st.write(doc_per_disc)
-        with col2:
-            st.subheader("Document Revision count")
-            st.write(doc_3_rev)
-        # st.write(df1)
+    st.write("Total Documents count", total_docs_count)
+    st.write("Total No of discipline", total_disc)
+    st.write("Total documents submitted", total_docs_submitted, "--> ",
+             (round((total_docs_submitted/total_docs_count)*100,1)), "%")
+    st.write("Total Approved documents", total_docs_approved)
+    st.write("Total Documents with more than 3 revisions",doc_3_rev_total)
+    st.write("rejected",total_docs_rejected)
+    col1, col2 = st.columns(2, gap="small")
+    with col1:
+        st.subheader("Documents per discipline")
+        st.write(doc_per_disc)
+    with col2:
+        st.subheader("Document Revision count")
+        st.write(doc_3_rev)
+    # st.write(df1)
 
 #Invoices data
 
